e8ec3f7f44765ec18df661810a79fa11ad3d516d
sanyath007 <sanyath007@gmail.com>
2023-01-27 14:46:00 +0700

    Install moment package and create User function in users/index.tsx and separate code from existed Users component to its

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import { GetServerSideProps } from "next"
+import Link from "next/link"
 import { useSession, getSession } from 'next-auth/react'
 import prisma from "@/prisma"
-import Link from "next/link"
+import moment from 'moment'
 
 export const getServerSideProps: GetServerSideProps = async () => {
     const res = await fetch('http://localhost:3000/api/users')
@@ -12,20 +13,30 @@ export const getServerSideProps: GetServerSideProps = async () => {
 }
 
 export default function Users (props: any) {
-    console.log(props.users);
-    
     return (
-        <div className="container px-2">
+        <div className="container px-4">
             <h1 className="text-3xl font-bold py-4">User List</h1>
-            <ul>
-                {props.users && props.users.map((user: any) => (
-                    <li key={user.id}>
-                        <Link href={`/users/${user.id}`}>
-                            <span className="text-gray-800 hover:text-gray-600">{user.name}</span>
-                        </Link>
-                    </li>
-                ))}
+
+            <ul className="w-3/4">
+                {props.users && props.users.map((user: any) => <User key={user.id} user={user} />)}
             </ul>
         </div>
+    )
+}
+
+function User({ user }: { user: any }) {
+    const formatDate = (date: string): string => {
+        return moment(user.createdAt).format('DD/MM/YYYY');
+    }
+
+    return (
+        <li>
+            <Link href={`/users/${user.id}`}>
+                <div className="flex justify-between w-full border py-2 px-4 text-gray-800 hover:text-gray-600 rounded-full">
+                    <span>{user.name}</span>
+                    <span>{formatDate(user.createdAt)}</span>
+                </div>
+            </Link>
+        </li>
     )
 }
